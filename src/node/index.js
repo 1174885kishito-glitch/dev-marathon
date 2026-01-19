6a01ddd2344fe58051c83e669f82663b5639b161
toshiki kobayashi <xiaolinjunhui0@gmail.com>
2026-01-19 16:29:01 +0900

    Fix config for staging environment

--- src/node/index.js
+++ src/node/index.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "user_5454", // PostgreSQLのユーザー名に置き換えてください
-  host: "db",
-  database: "crm_5454", // PostgreSQLのデータベース名に置き換えてください
-  password: "pass_5454", // PostgreSQLのパスワードに置き換えてください
+  user: "user_toshiki_kobayashi", // PostgreSQLのユーザー名に置き換えてください
+  host: "localhost",
+  database: "db_toshiki_kobayashi", // PostgreSQLのデータベース名に置き換えてください
+  password: "5Rw5YDaWc5jc", // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
--- src/web/config.js
+++ src/web/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: 'http://localhost:5454'
+  apiUrl: '/api_toshiki_kobayashi'
 };
 
 export default config;
